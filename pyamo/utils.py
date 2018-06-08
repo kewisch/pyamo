d8e5a64f7f967862987ed277378b3c6bcf19dc9d
KOLANICH <kolan_n@mail.ru>
2018-06-08 22:07:49 +0300

    HTTP/2 support

--- pyamo/utils.py
+++ pyamo/utils.py
@@ -10,6 +10,19 @@ import re
 import sys
 import requests
 
+try:
+    from hyper.contrib import HTTP20Adapter
+    hyperHttp2Adapter=HTTP20Adapter()
+except:
+    hyperHttp2Adapter=None
+
+class H2RequestsSession(requests.Session):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if hyperHttp2Adapter:
+            self.mount("https://", hyperHttp2Adapter)
+            self.mount("http://", hyperHttp2Adapter)
+
 from six.moves.configparser import ConfigParser, NoOptionError, NoSectionError
 from pytz import timezone
 from mozrunner import FirefoxRunner
